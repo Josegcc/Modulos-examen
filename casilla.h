9aa042d3f505559cfbbc5f86ff5a06f40cd76734
Josegcc <josegc356@gmail.com>
2026-06-19 13:39:57 -0400

    Add files via upload

--- casilla.h
+++ casilla.h
@@ -5,42 +5,50 @@ void casilla(int base, int altura, int posX, int posY)
     {
         gotoxy(posX, posY+i);
 
-        printf("c", 186);
-
-        if(i == 1)
-        {
-        
-            for(int j = 0; j <= base; j ++)
-            {
-            	if(j == 0){
-            	printf("%c", 201);	//Esquina superior izquierda
-            	}else if (j == base){
-            	printf("%c", 187);	//Esquina superior izquierda
-            	}else{
-            	printf("%c", 205);	//Lineas horizontales
-            	}
-            }
-            
-        }else if(i == altura){
-        		if(j == 0){
-            	printf("%c", 200);	//Esquina superior izquierda
-            	}else if (j == base){
-            	printf("%c", 188);	//Esquina superior izquierda
-            	}else{
-            	printf("%c", 205);	//Lineas horizontales
-            	}
-        }
-        else
-        {
-            for(int j = 0; j < base; j ++)
-            {
-            printf(" ");
-            }
-        }
-        
+		if(i != 1 && i != altura)
+		{
+		printf("%c", 186);	
+		}
         
 
-        printf("|");
+			if(i == 1)
+			{
+			
+				for(int j = 0; j <= base; j ++)
+				{
+					if(j == 0){
+					printf("%c", 201);	//Esquina superior izquierda
+					}else if (j == base){
+					printf("%c", 187);	//Esquina superior izquierda
+					}else{
+					printf("%c", 205);	//Lineas horizontales
+					}
+				}
+				
+			}else if(i == altura){
+				for(int j = 0; j <= base; j ++)
+				{
+					if(j == 0){
+					printf("%c", 200);	//Esquina superior izquierda
+					}else if (j == base){
+					printf("%c", 188);	//Esquina superior izquierda
+					}else{
+					printf("%c", 205);	//Lineas horizontales
+					}
+				}
+			}
+			else
+			{
+				for(int j = 0; j < base-1; j ++)
+				{
+				printf(" ");
+				}
+				printf("%c", 186);
+			}
+        
+        
+
+        
         printf("\n");
     }
 
